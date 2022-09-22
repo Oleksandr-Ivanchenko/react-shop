a9de9edb0519d26bba96712072b9a0fd183563f3
Oleksandr Ivanchenko <aleksandroc92@gmail.com>
2022-09-22 08:01:47 +0300

    package fixes

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path='/good/:id' element={<GoodItem />} />
           <Route path='/' element={<Home />} />
-          <Route path='*' element={<NotFoundPage />} />
+          {/* <Route path='*' element={<NotFoundPage />} /> */}
         </Routes>
       </Provider>
   );
